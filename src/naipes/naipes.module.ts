6867f93521f3504971a6f685bf1183fe34078286
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-09 14:23:32 -0300

    Correções nos arquivos

--- src/naipes/naipes.module.ts
+++ src/naipes/naipes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NaipesService } from './naipes.service';
 import { NaipesController } from './naipes.controller';
+import { PrismaModule } from '../prisma/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [NaipesController],
   providers: [NaipesService],
 })
