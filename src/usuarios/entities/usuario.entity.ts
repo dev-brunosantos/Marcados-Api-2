3dbf96b27b2940bf896f8ed44e382a18228062b9
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-09 15:09:34 -0300

    Criando a modelagem de entidade

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,11 @@
-export class Usuario {}
+export class Usuario {
+    id: string
+    nome: string
+    sobrenome?: string
+    email: string
+    senha: string
+    cargo: string
+    naipes: string
+    dtCadastro: Date
+    dtAtualizacao: Date
+}
