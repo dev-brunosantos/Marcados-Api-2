f517c05b8158f4c625b9b59f4645444054a2b166
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-09 13:03:15 -0300

    Criando o modulo e serviço do prisma para comunicação com a aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './escalas/usuarios/usuarios.module';
 import { CargosModule } from './cargos/cargos.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsuariosModule, CargosModule],
+  imports: [UsuariosModule, CargosModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
