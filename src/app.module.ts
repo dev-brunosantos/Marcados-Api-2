0bb8a99f834718cd8b49c0affb5394b48a018dc1
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-09 13:08:33 -0300

    Excluindo importações desnecessárias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsuariosModule } from './escalas/usuarios/usuarios.module';
 import { CargosModule } from './cargos/cargos.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsuariosModule, CargosModule, PrismaModule],
+  imports: [CargosModule],
   controllers: [AppController],
   providers: [AppService],
 })
