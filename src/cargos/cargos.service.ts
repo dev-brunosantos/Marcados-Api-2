6e89539322851b709e218c73362f7cf95c9a3ff2
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-09 13:20:25 -0300

    Criando o metodo responsável por filtrar um cargo especifos

--- src/cargos/cargos.service.ts
+++ src/cargos/cargos.service.ts
@@ -8,21 +8,21 @@ export class CargosService {
 
   constructor(
     private prisma: PrismaService
-  ) {}
+  ) { }
 
   async Criar(createCargoDto: CreateCargoDto) {
     const cargoExistente = await this.prisma.cargos.findFirst({
       where: { cargo: createCargoDto.cargo }
-    }) 
+    })
 
-    if(!cargoExistente) {
+    if (!cargoExistente) {
       const novoCargo = await this.prisma.cargos.create({
         data: {
           cargo: createCargoDto.cargo
         }
       })
 
-      return { message: `O cargo ${novoCargo.cargo} foi cadastrado com sucesso.`}
+      return { message: `O cargo ${novoCargo.cargo} foi cadastrado com sucesso.` }
     }
 
     throw new HttpException("O cargo informado ja existe no sistema.", HttpStatus.BAD_REQUEST)
@@ -31,14 +31,22 @@ export class CargosService {
   async ListarCargos() {
     const cargos = await this.prisma.cargos.findMany()
 
-    if(cargos.length === 0) {
+    if (cargos.length === 0) {
       throw new HttpException("Não existe nenhum cargo cadastrado no sistema.", HttpStatus.NOT_FOUND)
     }
     return cargos;
   }
+  
+  async FiltrarCargo(id: number) {
+    const cargoId = await this.prisma.cargos.findFirst({
+      where: { id }
+    })
+    
+    if(!cargoId) {
+      throw new HttpException("O ID informado não esta vinculado a nenhum cargo cadastrado no sistema.", HttpStatus.NOT_FOUND)
+    }
 
-  FiltrarCargo(id: number) {
-    return `This action returns a #${id} cargo`;
+    return cargoId;
   }
 
   AtualizarCargo(id: number, updateCargoDto: UpdateCargoDto) {
