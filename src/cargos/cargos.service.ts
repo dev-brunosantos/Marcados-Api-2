f5678ea07c3f50b09b75e15fcc07521383426ba2
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-09 13:17:29 -0300

    Criando o metodo de cadastro de cargos

--- src/cargos/cargos.service.ts
+++ src/cargos/cargos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCargoDto } from './dto/create-cargo.dto';
 import { UpdateCargoDto } from './dto/update-cargo.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -10,8 +10,22 @@ export class CargosService {
     private prisma: PrismaService
   ) {}
 
-  Criar(createCargoDto: CreateCargoDto) {
-    return 'This action adds a new cargo';
+  async Criar(createCargoDto: CreateCargoDto) {
+    const cargoExistente = await this.prisma.cargos.findFirst({
+      where: { cargo: createCargoDto.cargo }
+    }) 
+
+    if(!cargoExistente) {
+      const novoCargo = await this.prisma.cargos.create({
+        data: {
+          cargo: createCargoDto.cargo
+        }
+      })
+
+      return { message: `O cargo ${novoCargo.cargo} foi cadastrado com sucesso.`}
+    }
+
+    throw new HttpException("O cargo informado ja existe no sistema.", HttpStatus.BAD_REQUEST)
   }
 
   ListarCargos() {
