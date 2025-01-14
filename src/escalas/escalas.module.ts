263b1247f2f302b4aef1bd8715f9f61027ae7c97
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-14 17:26:53 -0300

    Criando o modulo de escalas

--- src/escalas/escalas.module.ts
+++ src/escalas/escalas.module.ts
@@ -1,7 +1,7 @@
+import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { EscalasService } from './escalas.service';
 import { EscalasController } from './escalas.controller';
-import { PrismaModule } from './../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
