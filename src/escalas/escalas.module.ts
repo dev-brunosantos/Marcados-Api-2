b5842faa4970eb961470e888d6fd7644ea95c65b
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-17 15:57:15 -0300

    Adicionando o modulo de funções no modulo de escalas

--- src/escalas/escalas.module.ts
+++ src/escalas/escalas.module.ts
@@ -2,9 +2,10 @@ import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { EscalasService } from './escalas.service';
 import { EscalasController } from './escalas.controller';
+import { FunctionsModule } from 'src/functions/functions.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, FunctionsModule],
   controllers: [EscalasController],
   providers: [EscalasService],
 })
