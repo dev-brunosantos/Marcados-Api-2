9721226ead1ae1cfd8fefb04b8cdae7adef4328f
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-22 15:42:20 -0300

    Atualizando o codigo com as novas funcionalidades

--- src/escalas/escalas.service.ts
+++ src/escalas/escalas.service.ts
@@ -15,8 +15,9 @@ export class EscalasService {
   async NovaEscala(createEscalaDto: CreateEscalaDto) {
     const escala1 = await this.escalas.CriarModelo(createEscalaDto);
     const escala2 = await this.escalas.CriarModelo(createEscalaDto);
-  
+
     const escala = {
+      ministro: `${escala1.ministro}`,
       sopranos: `${escala1.soprano}, ${escala2.soprano}`, 
       contraltos: `${escala1.contralto}, ${escala2.contralto}`, 
       tenores: `${escala1.tenor}, ${escala2.tenor}`, 
