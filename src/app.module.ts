c2f7ab5047884ba76cd356c4ede0c3a8d32ef62b
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-22 17:40:55 -0300

    Disponibilizando o uso do modulo de louvores no arquivo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CargosModule } from './cargos/cargos.module';
 import { NaipesModule } from './naipes/naipes.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { EscalasModule } from './escalas/escalas.module';
+import { LouvoresModule } from './louvores/louvores.module';
 
 @Module({
-  imports: [CargosModule, NaipesModule, UsuariosModule, EscalasModule],
+  imports: [CargosModule, NaipesModule, UsuariosModule, EscalasModule, LouvoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
