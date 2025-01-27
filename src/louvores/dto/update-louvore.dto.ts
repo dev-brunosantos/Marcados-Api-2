4faa1677161904d1843b837ffb01e50829578382
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-27 14:28:58 -0300

    Alteração no nome dos arquivos

--- src/louvores/dto/update-louvore.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLouvoreDto } from './create-louvore.dto';
-
-export class UpdateLouvoreDto extends PartialType(CreateLouvoreDto) {}
--- src/louvores/entities/louvore.entity.ts
+++
@@ -1,7 +0,0 @@
-export class Louvores {
-    id: number
-    nome: string 
-    link?: string
-    dtCadastro: Date
-    dtAtualizacao?: Date
-}
