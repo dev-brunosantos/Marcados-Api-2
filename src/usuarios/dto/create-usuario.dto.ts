0128ec54aac7ccd0ee02574c448c36a7bbf24c60
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-30 18:18:51 -0300

    Adicionando o class-validator no DTO criação de usuários

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,9 +1,30 @@
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+
 export class CreateUsuarioDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3, { message: 'I campo campo NOME deve conter no mínino 3 caracteres.'})
     nome: string
+    
+    @IsString()
+    @IsOptional()
     sobrenome?: string
+
+    @IsString()
+    @IsNotEmpty()
     email: string
+    
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6, { message: 'A senha deve conter no mínino 6 caracteres.'})
     senha: string
+    
+    @IsString()
+    @IsNotEmpty()
     cargo: string
+    
+    @IsString()
+    @IsNotEmpty()
     naipe: string
     dtCadastro: Date
     dtAtualizacao: Date
