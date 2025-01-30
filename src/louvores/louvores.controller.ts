754cd1cbedfafa0896877890de548792a041aedc
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-30 17:54:09 -0300

    Renomeando o nome dos metodos do controller de Louvores

--- src/louvores/louvores.controller.ts
+++ src/louvores/louvores.controller.ts
@@ -8,27 +8,27 @@ export class LouvoresController {
   constructor(private readonly louvoresService: LouvoresService) {}
 
   @Post()
-  create(@Body() createLouvoreDto: CreateLouvoreDto) {
+  Cadastrar(@Body() createLouvoreDto: CreateLouvoreDto) {
     return this.louvoresService.Criar(createLouvoreDto);
   }
 
   @Get()
-  findAll() {
+  Listar() {
     return this.louvoresService.Listar();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  LouvorID(@Param('id') id: string) {
     return this.louvoresService.BuscarLouvorID(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLouvoreDto: UpdateLouvoreDto) {
+  Atualizar(@Param('id') id: string, @Body() updateLouvoreDto: UpdateLouvoreDto) {
     return this.louvoresService.Atualizar(+id, updateLouvoreDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  Apagar(@Param('id') id: string) {
     return this.louvoresService.Apagar(+id);
   }
 }
