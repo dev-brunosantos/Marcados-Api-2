1c4dc0104030ae80bf572ccad2671748eda30115
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-30 18:21:28 -0300

    Adicionando o class-validator no DTO de atualização de usuários

--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,11 +1,28 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+    @IsString()
+    @IsOptional()
     nome: string
+
+    @IsString()
+    @IsOptional()
     sobrenome?: string
+
+    @IsString()
+    @IsOptional()
+    @MinLength(6, { message: 'A senha deve conter no mínino 6 caracteres.'})
     senha: string
+
+    @IsString()
+    @IsOptional()
     cargo: string
+
+    @IsString()
+    @IsOptional()
     naipe: string
+    
     dtAtualizacao: Date
 }
